2134bf072a319f406c01f12c46a83a54ccb1f87d
prosi100 <prosi100@mail.chapman.edu>
2017-04-07 11:52:31 -0700

    Create TreeNode.cpp

---
+++ TreeNode.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "BST.h"
+
+using namespace std;
+
+TreeNode::TreeNode()
+{
+	left = NULL;
+	right = NULL;
+}
+TreeNode::TreeNode(int k)
+{
+	left = NULL;
+	right = NULL;
+	key = k;
+}
