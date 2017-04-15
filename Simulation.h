f1ee3ec6f42297dd1c7ee76ccae20e2dad58d442
prosi100 <prosi100@mail.chapman.edu>
2017-04-15 13:52:44 -0700

    Update Simulation.h

--- Simulation.h
+++ Simulation.h
@@ -14,11 +14,23 @@ class Simulation
 		void option2();
 		void option3();
 		void option4();
-		void option7();
-		void option9();
+		void option5();
+		void option6();
+		void option7(); 
+		void option8(); 
+		void option9(); 
+		void option10(); 
+		void option11(); 
+		void option12(); 
+		void option13();
+		BST<Student> getMasterStudent();
+		BST<Advisor> getMasterAdvisor();
+
 	private:
 		BST<Student> masterStudent;
 		BST<Advisor> masterFaculty;
+		GenStack<Student> rollbackStd;
+		GenStack<Advisor> rollbackAd;
 };
 
 Simulation::Simulation(BST<Student> st,BST<Advisor> ad)
@@ -125,7 +137,37 @@ void Simulation::option4()
 			goodValue=false;
 		}
 	}while(!goodValue);
-	if(masterFaculty.contains(Advisor(theID)))
+	if(masterFaculty.contains(anAdvisor))
+	{
+		cout<<"out"<<endl;
+		Advisor theAdvisor = masterFaculty.getNode(Advisor(theID));
+		cout<<theAdvisor<<endl;
+	}
+	else
+	{
+		cout<<"This faculty member is not in the system."<<endl;
+	}
+}
+void Simulation::option5()
+{
+	string inputString;
+	cout << "Enter the student's ID: " << endl;
+	int theID;
+	do//checks that the input is valid
+	{
+		goodValue = true;			
+		cin>>inputString;
+		int count = 0;
+		theID = checkIntegerInput(inputString);
+		if(theID==-1)
+		{
+			cout<<"Please enter a valid number"<<endl;
+			goodValue=false;
+		}
+	}while(!goodValue);
+	Advisor anAdvisor(theID);
+	cout<<anAdvisor<<endl; //print out the advisors info
+	if(masterFaculty.contains(anAdvisor))
 	{
 		Advisor theAdvisor = masterFaculty.getNode(Advisor(theID));
 		cout<<theAdvisor<<endl;
@@ -134,6 +176,27 @@ void Simulation::option4()
 	{
 		cout<<"This faculty member is not in the system."<<endl;
 	}
+}
+void Simulation::option6()
+{
+	string inputString;
+	cout << "Enter the faculty's ID: " << endl;
+	int theID;
+	do//checks that the input is valid
+	{
+		goodValue = true;			
+		cin>>inputString;
+		int count = 0;
+		theID = checkIntegerInput(inputString);
+		if(theID==-1)
+		{
+			cout<<"Please enter a valid number"<<endl;
+			goodValue=false;
+		}
+	}while(!goodValue);
+	Advisor anAdvisor(theID);
+	//print advisees
+	
 }
 
 void Simulation::option7()
@@ -216,6 +279,31 @@ void Simulation::option7()
 		masterFaculty.insertDataAtNode(theirAdvisor, theirAdvisor);
 	}
 }
+void Simulation::option8
+{
+	string inputString;
+	cout << "Enter the student's ID: " << endl;
+	int theID;
+	do//checks that the input is valid
+	{
+		goodValue = true;			
+		cin>>inputString;
+		int count = 0;
+		theID = checkIntegerInput(inputString);
+		if(theID==-1)
+		{
+			cout<<"Please enter a valid number"<<endl;
+			goodValue=false;
+		}
+	}while(!goodValue);
+	Student myStudent= masterStudent.getNode(Student(ID)); //makes student based off id
+	advisorID = myStudent.getAdvisorID(); //find advisor id 
+	Advisor myAdviosr = masterFaculty.getNode(Advisor(advisorID)); //find advisor in bst based off id
+	masterFaculty.deleteNode(masterFaculty Advisor(ID)); //delete student id from advisor 
+	masterStudent.deleteNode(masterStudent Student(ID)); //delete student
+	rollbackStd.push(myStudent);
+
+}
 
 void Simulation::option9()
 {
@@ -225,6 +313,7 @@ void Simulation::option9()
 	string level;
 	string department;
 	int id;	
+	//what do we do about the student IDs??????
 	cout<<"Please enter the faculty member's name: "<<endl;
 	cin>>name;
 	cout<<"Please enter the faculty member's level: "<<endl;
@@ -248,6 +337,124 @@ void Simulation::option9()
 	}while(!goodValue);
 
 	masterFaculty.insert(Advisor(name, level, department, id));
-
+	rollbackAd.push(Advisor(name, level, department, id));
+}
+void Simulation::option10()
+{
+	string inputString;
+	cout << "Enter the advisor's ID: " << endl;
+	int theID;
+	do//checks that the input is valid
+	{
+		goodValue = true;			
+		cin>>inputString;
+		int count = 0;
+		theID = checkIntegerInput(inputString);
+		if(theID==-1)
+		{
+			cout<<"Please enter a valid number"<<endl;
+			goodValue=false;
+		}
+	}while(!goodValue);
+	Advisor myAdvisor= masterAdvisor.getNode(Advisor(ID)); //makes advisor based off id
+	masterFaculty.deleteNode(masterFaculty Advisor(ID)); //delete advisor 
+	rollbackAd.push(myAdvisor);
 
 }
+void Simulation::option11()
+{
+	string inputString;
+	cout << "Enter the advisor's ID: " << endl;
+	int theID;
+	do//checks that the input is valid
+	{
+		goodValue = true;			
+		cin>>inputString;
+		int count = 0;
+		theID = checkIntegerInput(inputString);
+		if(theID==-1)
+		{
+			cout<<"Please enter a valid number"<<endl;
+			goodValue=false;
+		}
+	}while(!goodValue);
+	Advisor myAdvisor= masterAdvisor.getNode(Advisor(ID)); //makes advisor based off id
+	rollbackAd.push(myAdvisor);
+
+	cout << "Enter the student's ID: " << endl;
+	do//checks that the input is valid
+	{
+		goodValue = true;			
+		cin>>inputString;
+		int count = 0;
+		theID = checkIntegerInput(inputString);
+		if(theID==-1)
+		{
+			cout<<"Please enter a valid number"<<endl;
+			goodValue=false;
+		}
+	}while(!goodValue);
+	Student myStudent= masterStudent.getNode(Student(ID)); //makes student based off id
+	rollbackStd.push(myStudent);
+}
+void Simulation::option12()
+{
+
+	string inputString;
+	cout << "Enter the advisor's ID: " << endl;
+	int theID;
+	do//checks that the input is valid
+	{
+		goodValue = true;			
+		cin>>inputString;
+		int count = 0;
+		theID = checkIntegerInput(inputString);
+		if(theID==-1)
+		{
+			cout<<"Please enter a valid number"<<endl;
+			goodValue=false;
+		}
+	}while(!goodValue);
+	Advisor myAdvisor= masterAdvisor.getNode(Advisor(ID)); //makes advisor based off id
+	rollbackAd.push(myAdvisor);
+
+	cout << "Enter the student's ID: " << endl;
+	do//checks that the input is valid
+	{
+		goodValue = true;			
+		cin>>inputString;
+		int count = 0;
+		theID = checkIntegerInput(inputString);
+		if(theID==-1)
+		{
+			cout<<"Please enter a valid number"<<endl;
+			goodValue=false;
+		}
+	}while(!goodValue);
+	Student myStudent= masterStudent.getNode(Student(ID)); //makes student based off id
+	rollbackStd.push(myStudent);
+}
+void Simulation::option13()
+{
+	while (rollbackAd!=isEmpty())
+	{
+		masterFaculty.insert(rollbackAd.pop()); //pop advisor off stack and back into tree
+	}
+	while (rollbackStd!=isEmpty())
+	{
+		masterStudent.insert(rollbackStd.pop()); //pop student off stack and back into tree
+	}
+}
+
+
+BST<Student> Simulation::getMasterStudent()
+{
+	return masterStudent;
+}
+		
+BST<Advisor> Simulation::getMasterAdvisor()
+{
+	return masterFaculty;
+}
+
+
